1ead0434dcec1aea26d8e061e35847804999572b
Carlin <carlin.q.scott@gmail.com>
2017-04-21 18:34:27 -0700

    r key can play/pause on Pandora!

--- data/orchestrator.js
+++ data/orchestrator.js
@@ -6,92 +6,107 @@
  * Supports backwards compatibility with older Gecko key values.
  * See https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key
  */
-MediaKeys.Init = function()
-{
-	self.port.on("MediaPlay", function() {
+MediaKeys.Init = function () {
+	function MediaPlay(sendResponse) {
 		var playButton = MediaKeys.GetSingleElementByXpath(MediaKeys.playButton, MediaKeys.basePlayer);
 		if (playButton == null) return;
 		playButton.click();
-		self.port.emit("Play");
-	});
+		sendResponse("Play");
+	}
 
-	self.port.on("MediaPause", function(){
-		console.log("pausing " + window.URL);
+	function MediaPause(sendResponse) {
 		var pauseButton = MediaKeys.GetSingleElementByXpath(MediaKeys.pauseButton)
-		if (pauseButton != null)
-		{
+		if (pauseButton != null) {
 			pauseButton.click();
+			sendResponse("Pause");
 		}
-	});
-		
-    self.port.on("MediaPlayPause", function() {
-        var playButton = MediaKeys.GetSingleElementByXpath(MediaKeys.playButton, MediaKeys.basePlayer);
-        if (playButton != null)
-        {
-            playButton.click();
-            self.port.emit("Play");
-        }
-        else
-        {
-            var pauseButton = MediaKeys.GetSingleElementByXpath(MediaKeys.pauseButton, MediaKeys.basePlayer);
-            if (pauseButton == null) return;
-	    	pauseButton.click();
-            self.port.emit("Pause");
-        }
-	});
+	}
 
-	self.port.on("MediaTrackNext", function() {
-        var skipButton = MediaKeys.GetSingleElementByXpath(MediaKeys.skipButton, MediaKeys.basePlayer);
-        if (skipButton == null) return;
-        skipButton.click();
-        self.port.emit("Next");
-    });
+	function MediaPlayPause(sendResponse) {
+		var playButton = MediaKeys.GetSingleElementByXpath(MediaKeys.playButton, MediaKeys.basePlayer);
+		if (playButton != null) {
+			playButton.click();
+			sendResponse("Play");
+		}
+		else {
+			var pauseButton = MediaKeys.GetSingleElementByXpath(MediaKeys.pauseButton, MediaKeys.basePlayer);
+			if (pauseButton == null) return;
+			pauseButton.click();
+			sendResponse("Pause");
+		}
+	}
 
-    self.port.on("MediaTrackPrevious", function() {
-        var previousButton = MediaKeys.GetSingleElementByXpath(MediaKeys.previousButton, MediaKeys.basePlayer);
-        if (previousButton == null) return;
-        previousButton.click();
-        self.port.emit("Previous");
-	});
+	function MediaNextTrack(sendResponse) {
+		var skipButton = MediaKeys.GetSingleElementByXpath(MediaKeys.skipButton, MediaKeys.basePlayer);
+		if (skipButton == null) return;
+		skipButton.click();
+		sendResponse("Next");
+	}
 
-	self.port.on("MediaStop", function(){
+	function MediaPrevTrack(sendResponse) {
+		var previousButton = MediaKeys.GetSingleElementByXpath(MediaKeys.previousButton, MediaKeys.basePlayer);
+		if (previousButton == null) return;
+		previousButton.click();
+		sendResponse("Previous");
+	}
+
+	function MediaStop(sendResponse) {
 		var pauseButton = MediaKeys.GetSingleElementByXpath(MediaKeys.pauseButton, MediaKeys.basePlayer);
 		if (pauseButton == null) return;
 		pauseButton.click();
-		self.port.emit("Stop");
-	});
+		sendResponse("Stop");
+	}
 
-	if (MediaKeys.trackInfo && Notification.permission != 'denied')
-	{
-		var setupTrackInfoUpdates = function ()
-		{
-			function notifyNewTrack()
-			{
+	if (MediaKeys.trackInfo && Notification.permission != 'denied') {
+		var setupTrackInfoUpdates = function () {
+			function notifyNewTrack() {
 				new Notification("Now Playing", {
-					body: MediaKeys.GetSingleElementByXpath(MediaKeys.trackInfo, MediaKeys.basePlayer).innerText 
+					body: MediaKeys.GetSingleElementByXpath(MediaKeys.trackInfo, MediaKeys.basePlayer).innerText
 				});
 			}
-			
+
 			var currentTrackObservable;
-			
-			if (MediaKeys.trackInfoContainer) 
+
+			if (MediaKeys.trackInfoContainer)
 				currentTrackObservable = MediaKeys.GetSingleElementByXpath(MediaKeys.trackInfoContainer, MediaKeys.basePlayer);
-			else 
+			else
 				currentTrackObservable = MediaKeys.GetSingleElementByXpath(MediaKeys.trackInfo, MediaKeys.basePlayer);
 
-			if (currentTrackObservable){
+			if (currentTrackObservable) {
 				var currentTrackObserver = new MutationObserver(notifyNewTrack);
 				currentTrackObserver.observe(currentTrackObservable, {
 					childList: true,
 					characterData: true,
-					subtree: true 
+					subtree: true
 				});
 			}
 		}
 
 		if (Notification.permission == 'granted') setupTrackInfoUpdates();
-		else Notification.requestPermission().then(function(result) { if (result == 'granted') setupTrackInfoUpdates(); });
+		else Notification.requestPermission().then(function (result) { if (result == 'granted') setupTrackInfoUpdates(); });
 	}
+	browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+		switch (request) {
+			case "MediaPlayPause":
+				MediaPlayPause(sendResponse);
+				break;
+
+			case "MediaNextTrack":
+				MediaNextTrack(sendResponse);
+				break;
+
+			case "MediaPrevTrack":
+				MediaPrevTrack(sendResponse);
+				break;
+
+			case "MediaStop":
+				MediaStop(sendResponse);
+				break;
+
+			default:
+				break;
+		}
+	});
 };
 
 MediaKeys.Init();
