fbb6c75c493c4bccbc668888719fbaf4c5c3e2e1
Carlin <Carlin.Q.Scott@gmail.com>
2019-06-12 16:53:24 -0700

    deploying extension using "web-ext sign"

--- deploy/deploy-mozilla.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-artifactList=($DIR/../web-ext-artifacts/*.zip)
-
-token=$(node $DIR/auth-mozilla.js)
-curl "https://addons.mozilla.org/api/v4/addons/" \
-    -g -XPOST -F "upload=@$artifactList" \
-    -H "Authorization: JWT $token"
