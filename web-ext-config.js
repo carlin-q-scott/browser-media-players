90d06a6f85c46214a1959b551789c57eb5a4c727
Carlin <Carlin.Q.Scott@gmail.com>
2019-06-27 15:03:16 -0700

    unhid web-ext-config.js from web-ext cli

---
+++ web-ext-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    ignoreFiles: [
+        "jsconfig.json",
+        "package-lock.json",
+        "package.json",
+        "tests",
+        "data/pageMods.json",
+        "docs",
+        "icon425.png",
+        "web-ext-config.js",
+        "web-ext-artifacts"
+    ],
+    build: {
+        overwriteDest: true
+    }
+}
