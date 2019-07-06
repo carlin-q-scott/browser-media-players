4064c24d19389f567ec4292b80417f60a1b28d8f
Carlin <Carlin.Q.Scott@gmail.com>
2019-07-05 17:23:22 -0700

    added support for kanopy.com

---
+++ data/kanopy-view.js
@@ -0,0 +1,13 @@
+/**
+ * MediaKeys namespace.
+ */
+/**
+ * MediaKeys namespace.
+ */
+if (typeof MediaKeys == "undefined") var MediaKeys = {};
+
+MediaKeys.useCssSelectors = true;
+MediaKeys.playButton = "button.vjs-play-control.vjs-paused";
+MediaKeys.pauseButton = "button.vjs-play-control.vjs-playing";
+MediaKeys.skipButton = "div.item.active + div";
+MediaKeys.previousButton = "div.item.active - div";
