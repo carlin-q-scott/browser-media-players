2abe2a104338dea04364d1c1ae0a7cae8d0b3c03
Carlin <Carlin.Q.Scott@gmail.com>
2019-10-25 20:18:59 -0700

    fixed webpack errors caused by vestigial code

--- app/lib/hotkeyManager.js
+++ app/lib/hotkeyManager.js
@@ -1,17 +1,7 @@
-/* global console */
 import pageWorkerManager from './pageWorkerManager.js';
 import webExtensionCommands from './webExtensionCommands.js';
 
 var hotkeyWorker = null;
-var fellBackToFirefoxHotkeys = false;
-
-var RegistrationErrorHandler = function (error) {
-    console.warn(`falling back to web extension commands due to: "${error.message}" in ${error.filename}:${error.lineno}-${error.colno}`);
-    if (!fellBackToFirefoxHotkeys) {
-        fellBackToFirefoxHotkeys = true;
-        RegisterWebextensionCommands();
-    }
-};
 
 var RegisterWebextensionCommands = () => {
     hotkeyWorker = webExtensionCommands;
@@ -19,69 +9,19 @@ var RegisterWebextensionCommands = () => {
     hotkeyWorker.postMessage('attach');
 };
 
-var RegisterHotkeys = function()
+function RegisterHotkeys()
 {
     if (hotkeyWorker != null) return;
+    RegisterWebextensionCommands();
+}
 
-    //switch (browser.runtime.PlatformOs)
-    // always use webExtensionHotkeys for now; aka commands.
-    switch ('default')
-    {
-        case 'win':
-            console.log('Registering global hotkeys');
-            if (require('sdk/simple-prefs').prefs.UseMkWin){
-                hotkeyWorker = require('./mk-win.js');
-                hotkeyWorker.addEventListener(pageWorkerManager.EmitEventToActivePageWorker);
-                hotkeyWorker.onerror = RegistrationErrorHandler;
-                //hotkeyWorker.postMessage("debug");
-            }
-            else{
-                var {Cu} = require('chrome');       
-                var {ChromeWorker} = Cu.import('resource://gre/modules/Services.jsm', null);
-                hotkeyWorker = new ChromeWorker(require('sdk/self').data.url('../lib/windowsHotkeys.js'));
-                hotkeyWorker.addEventListener('message', pageWorkerManager.EmitEventToActivePageWorker);
-                hotkeyWorker.onerror = RegisterWebextensionCommands;
-            }
-            hotkeyWorker.postMessage('attach');
-            break;
-        case 'linux':
-            console.log('Registering DBus hotkeys');
-            hotkeyWorker = require('./linuxDBusHotkeys');
-            hotkeyWorker.addEventListener(pageWorkerManager.EmitEventToActivePageWorker);
-            if (hotkeyWorker.gLibsExist) {
-                try
-                {
-                    hotkeyWorker.postMessage('attach');
-                }
-                catch (exception)
-                {
-                    RegisterWebextensionCommands();
-                }
-                break;
-            } else {
-                console.log('DBus not supported. glib, gobject and gio libaries are required.');
-            }
-        case 'mac':
-            console.log('Registering global hotkeys');
-            hotkeyWorker = require('./darwinKeys');
-            hotkeyWorker.addEventListener(pageWorkerManager.EmitEventToActivePageWorker);
-            hotkeyWorker.postMessage('attach');
-            break;
-        default:
-            // console.log(`Global hotkeys not supported for ${browser.runtime.PlatformOs}. Falling back to browser hotkeys`);
-            RegisterWebextensionCommands();
-    }
-
-};
-
-var UnregisterHotkeys = function(){
+function UnregisterHotkeys(){
     if (hotkeyWorker != null){
         hotkeyWorker.postMessage('detach');
         hotkeyWorker.removeEventListener('message', pageWorkerManager.EmitEventToActivePageWorker);
         hotkeyWorker = null;
-        fellBackToFirefoxHotkeys = false;
     }
-};
+}
 
 export default {
     RegisterHotkeys,
