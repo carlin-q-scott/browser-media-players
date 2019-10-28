140a09486001e7c865f44099797aec462c8bd14a
Carlin <Carlin.Q.Scott@gmail.com>
2019-10-28 09:54:40 -0700

    added es-lint to the build process

---
+++ webextension-toolbox-config.js
@@ -0,0 +1,19 @@
+// This file is not going through babel transformation.
+// So, we write it in vanilla JS
+// (But you could use ES2015 features supported by your Node.js version)
+/* eslint-env commonjs */
+// const webpack = require('webpack')
+
+module.exports = {
+    // eslint-disable-next-line no-unused-vars
+    webpack: (config, { dev, vendor }) => {
+    // Perform customizations to webpack config
+        config.module.rules.unshift({
+            test: /\.(js)$/,
+            exclude: /node_modules/,
+            use: 'eslint-loader'
+        })
+        // Important: return the modified config
+        return config
+    }
+}
