db18efab02ef0b387430c7c4ff21e8fffca7333c
Luca Pasquale <luca.pasquale@moxoff.com>
2019-12-09 13:20:27 +0100

    updated css selectors for deezer

--- data/deezer.com-view.js
+++ data/deezer.com-view.js
@@ -3,7 +3,7 @@
  */
 if (typeof MediaKeys == 'undefined') var MediaKeys = {};
 let basePlayer = 'div#page_player ';
-MediaKeys.playButton = basePlayer + 'button .icon-play';
-MediaKeys.pauseButton = basePlayer + 'button .icon-pause';
-MediaKeys.skipButton = basePlayer + 'button .icon-next';
-MediaKeys.previousButton = basePlayer + 'button .icon-prev';
+MediaKeys.playButton = basePlayer + 'button .svg-icon-play';
+MediaKeys.pauseButton = basePlayer + 'button .svg-icon-pause';
+MediaKeys.skipButton = basePlayer + 'button .svg-icon-next';
+MediaKeys.previousButton = basePlayer + 'button .svg-icon-prev';
