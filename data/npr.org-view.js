c23ffdbeeca8fad75bd4b25699dc0f1441c5b44b
Carlin <Carlin.Q.Scott@gmail.com>
2020-01-04 13:24:37 -0800

    added npr.org

---
+++ data/npr.org-view.js
@@ -0,0 +1,7 @@
+if (typeof MediaKeys == 'undefined') var MediaKeys = {};
+
+MediaKeys.player = '.npr-player';
+MediaKeys.playButton = '.icn-play';
+MediaKeys.pauseButton = '.icn-pause';
+MediaKeys.nextButton = 'player-skip';
+MediaKeys.previousButton = 'player-previous'
