d652d91e1b17f2000c7779a09fa7aac0f48d7318
Carlin <Carlin.Q.Scott@gmail.com>
2020-02-02 12:11:35 -0800

    fix issue 167: Udemy support

--- data/udemy.com-view.js
+++ data/udemy.com-view.js
@@ -3,7 +3,7 @@
  */
 if (typeof MediaKeys == 'undefined') var MediaKeys = {};
 
-MediaKeys.playButton = 'button.paused';
-MediaKeys.pauseButton = 'button.playing';
-MediaKeys.skipButton = 'a.forward';
-MediaKeys.previousButton = 'a.rewind';
+MediaKeys.playButton = 'button[data-purpose=play-button]';
+MediaKeys.pauseButton = 'button[data-purpose=pause-button]';
+MediaKeys.skipButton = 'button[data-purpose=forward-skip-button]';
+MediaKeys.previousButton = 'button[data-purpose=rewind-skip-button]';
